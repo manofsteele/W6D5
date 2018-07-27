86dc00d70c228a7406794e958496db15a36fa072
App Academy Student <appacademy@Apps-Mac-mini-110.local>
2018-07-27 15:11:53 -0700

    Finish phase 2 of Widgets -- the clock

---
+++ widgets/frontend/clock.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+  }
+
+  tick(){
+    this.setState({
+      time: new Date()
+    });
+  }
+
+
+  render(){
+    let options = {year: 'numeric', month: 'long', day: 'numeric'};
+    return (
+
+      <div className="clock">
+        <div className="time">
+          <label>Current Time</label>
+          <p class="clock-right">{this.state.time.toLocaleTimeString('it-IT')}</p>
+        </div>
+        <br></br>
+        <div>
+          <label>Today's Date</label>
+          <p class="clock-right">{this.state.time.toLocaleDateString('en-US', options)}</p>
+        </div>
+
+      </div>
+    );
+  }
+
+  componentDidMount(){
+    this.intervalId = setInterval(this.tick.bind(this), 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+  }
+}
+
+export default Clock;
